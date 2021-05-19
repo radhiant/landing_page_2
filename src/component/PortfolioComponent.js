2f4ffd99bf85dd658a05123ec59073e0560f59c9
radhiant <radhiantsobarna@gmail.com>
2021-05-19 14:15:44 +0700

    update code

--- src/component/PortfolioComponent.js
+++ src/component/PortfolioComponent.js
@@ -8,26 +8,26 @@ export default class PortfolioComponent extends Component {
                     <br /><br /><br />
                     <div className="row justify-content-center">
                         <div className="col-lg-4 col-md-6">
-                            <div className="card mb-4" data-aos="zoom-in" data-aos-duration="600">
+                            <div className="card mb-4" data-aos="fade-up" data-aos-duration="600">
                                 <div className="card-body p-0">
                                     <img src="assets/portofolio/p-1.png" width="100%" alt="img" />
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6">
-                            <div className="card" data-aos="zoom-in" data-aos-duration="600">
+                            <div className="card" data-aos="fade-up" data-aos-duration="600">
                                 <div className="card-body p-0">
                                     <img src="assets/portofolio/p-2.png" width="100%" alt="img" />
                                 </div>
                             </div>
-                            <div className="card mt-4 mb-4" data-aos="zoom-in" data-aos-duration="600">
+                            <div className="card mt-4 mb-4" data-aos="fade-up" data-aos-duration="600">
                                 <div className="card-body p-0">
                                     <img src="assets/portofolio/p-3.png" width="100%" alt="img" />
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6">
-                            <div className="card" data-aos="zoom-in" data-aos-duration="600">
+                            <div className="card" data-aos="fade-up" data-aos-duration="600">
                                 <div className="card-body p-0">
                                     <img src="assets/portofolio/p-4.png" width="100%" alt="img" />
                                 </div>
@@ -38,7 +38,7 @@ export default class PortfolioComponent extends Component {
 
                         <div className="col-lg-3">
                             <center>
-                                <a href="#all" className="btn btn-white p-3 mt-5 rounded-pill shadow-lg" data-aos="zoom-in" data-aos-duration="400">VIEW ALL PROJECT</a>
+                                <a href="#all" className="btn btn-white p-3 mt-5 rounded-pill shadow-lg" data-aos="fade-down" data-aos-duration="400">VIEW ALL PROJECT</a>
                             </center>
                         </div>
                     </div>
