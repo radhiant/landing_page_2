14ddacdf5f9ef8550eb86dd55e2c62fb0e16ba89
radhiant <radhiantsobarna@gmail.com>
2021-05-19 14:10:57 +0700

    update code

--- src/component/PortfolioComponent.js
+++ src/component/PortfolioComponent.js
@@ -10,26 +10,26 @@ export default class PortfolioComponent extends Component {
                         <div className="col-lg-4 col-md-6">
                             <div className="card mb-4" data-aos="zoom-in" data-aos-duration="600">
                                 <div className="card-body p-0">
-                                    <img src="assets/portofolio/p-1.svg" width="100%" alt="img" />
+                                    <img src="assets/portofolio/p-1.png" width="100%" alt="img" />
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6">
                             <div className="card" data-aos="zoom-in" data-aos-duration="600">
                                 <div className="card-body p-0">
-                                    <img src="assets/portofolio/p-2.svg" width="100%" alt="img" />
+                                    <img src="assets/portofolio/p-2.png" width="100%" alt="img" />
                                 </div>
                             </div>
                             <div className="card mt-4 mb-4" data-aos="zoom-in" data-aos-duration="600">
                                 <div className="card-body p-0">
-                                    <img src="assets/portofolio/p-3.svg" width="100%" alt="img" />
+                                    <img src="assets/portofolio/p-3.png" width="100%" alt="img" />
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6">
                             <div className="card" data-aos="zoom-in" data-aos-duration="600">
                                 <div className="card-body p-0">
-                                    <img src="assets/portofolio/p-4.svg" width="100%" alt="img" />
+                                    <img src="assets/portofolio/p-4.png" width="100%" alt="img" />
                                 </div>
                             </div>
                         </div>
